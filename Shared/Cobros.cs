43dca5a7cbe503fc337c2aff3a9235cfdec45117
darcklito <luis_paulino5@ucne.edu.do>
2023-11-20 22:42:20 -0400

    Add project files.

---
+++ Shared/Cobros.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuisJose_AP1_P2_Real.Shared
+{
+    internal class Cobros
+    {
+    }
+}
