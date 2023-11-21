6d666faaa5e326138942b84ea345f5b92d1ce239
darcklito <luis_paulino5@ucne.edu.do>
2023-11-21 00:59:03 -0400

    modificaciones a la tabla de CobrosDetalle

---
+++ Shared/CobrosDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuisJose_AP1_P2_Real.Shared
+{
+    public class CobrosDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public int VentaId { get; set; }
+        public int CobroId { get; set; }
+        public int ClienteId { get; set; }
+        public double Cobrado { get; set; }
+        public bool Pagado { get; set; }
+    }
+}
