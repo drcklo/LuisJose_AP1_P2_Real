a2bcb4b32e011e21c497345b32a55ac2ffa85fc9
darcklito <luis_paulino5@ucne.edu.do>
2023-11-21 00:58:20 -0400

    modificaciones a la tabla de ventas

---
+++ Shared/Ventas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuisJose_AP1_P2_Real.Shared
+{
+    public class Ventas
+    {
+        [Key]
+        public int VentaId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int ClienteId { get; set; }
+        public double Monto { get; set; }
+        public double Balance { get; set; }
+    }
+}
