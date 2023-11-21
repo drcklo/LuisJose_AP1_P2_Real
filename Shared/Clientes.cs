0013221cd47b0308246e0f46889142831ff8bac5
darcklito <luis_paulino5@ucne.edu.do>
2023-11-20 22:51:41 -0400

    creacion de tabla de clientes

---
+++ Shared/Clientes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuisJose_AP1_P2_Real.Shared
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+    }
+}
